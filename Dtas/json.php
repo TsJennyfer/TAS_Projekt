eea2c3859788184d1de4c5fed250ad466019bbe3
TsJennyfer <TsJennyfer@users.noreply.github.com>
2017-12-03 21:52:01 +0100

    clean up + json changes

--- Dtas/json.php
+++ Dtas/json.php
@@ -1,18 +1,23 @@
 <?php
     //open connection to mysql db
-    $connection = mysqli_connect("localhost","root","","api") or die("Error " . mysqli_error($connection));
+    $connection = mysqli_connect("localhost","root","root","api") or die("Error " . mysqli_error($connection)); //3d parametr - password
 
+    $sql = "set names utf8";
+    
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+    
     //fetch table rows from mysql db
     $sql = "select * from ksiazki";
+    
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
-
+    
     //create an array
     $emparray = array();
     while($row =mysqli_fetch_assoc($result))
     {
         $emparray[] = $row;
     }
-    echo json_encode($emparray);
+    echo json_encode(["result"=>"OK", "data"=>$emparray]);
 
     //close the db connection
     mysqli_close($connection);
--- myapp.js
+++ myapp.js
@@ -1,41 +1,36 @@
 function myfunction(e)
 {
-    e.preventDefault();
-    var myText = document.getElementById("textbox").value;
-    document.getElementById('myDiv').innerHTML = myText;
-    var myData = JSON.parse(myText);
-    console.log(myData);
+        e.preventDefault();
+        var myText = document.getElementById("textbox").value;
+        document.getElementById('myDiv').innerHTML = myText;
+        var myData = JSON.parse(myText);
+        console.log(myData);
 }
 
 function readServer()
 {
-   // alert("readServer!!!");
-
-  // readServerData("file:///Users/jn/Documents/work/data.json");
-    
-    readServerData();
+        readServerData("json.php");
+        readServerData();
 }
 
-/*function readServerData(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        alert("!!!");
-
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
 function readServerData(file)
+{
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function ()
+        {
+                if(rawFile.readyState === 4)
+                {
+                        if(rawFile.status === 200 || rawFile.status == 0)
+                        {
+                                var allText = rawFile.responseText;
+                                reanderMyData(allText);
+                        }
+                }
+        }
+        rawFile.send(null);
+}
+/*function readServerData(file)
 {
         
         var reader = new FileReader();
@@ -46,19 +41,28 @@ function readServerData(file)
             console.log(text);
         }
         reader.readAsText(file);
- }*/
+ }
 
 function readServerData()
 {
     var data = '{"index1":"value1", "index2":[1,2,3]}';
-    var myData = JSON.parse(data);
-    console.log(myData);
-    reanderMyData(myData);
+    
 }
-
-function reanderMyData(myData)
+*/
+function reanderMyData(data)
 {
-    for(var x in myData)
-        console.log(x);
-  
+        var myData = JSON.parse(data);
+        var html;
+        if(myData["result"] == "OK")
+        {
+                console.log(myData);
+                for(var x in myData){
+                        console.log(x); 
+                        
+                }
+                html = "<button>aaa</button>";  //make html 
+                document.getElementById("app").innerHTML = html;
+        }
+        else allert("error");
+                
 }
