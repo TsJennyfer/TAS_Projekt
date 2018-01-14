4d28f31e6e3aa5d08da2889f8b11ae47a1f6883c
TsJennyfer <TsJennyfer@users.noreply.github.com>
2018-01-14 12:25:18 +0100

    main changes

--- Dtas/config/database.php
+++ Dtas/config/database.php
@@ -5,7 +5,7 @@ class Database{
     private $host = "localhost";
     private $db_name = "api";
     private $username = "root";
-    private $password = "";
+    private $password = "root";
     public $conn;
  
     
